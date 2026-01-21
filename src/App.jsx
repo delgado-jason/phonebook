cd931e239dfefe81af8231d318b7c8bbdcf17349
delgado-jason <delgadorjason7@gmail.com>
2026-01-21 15:03:27 -0600

    Move Seed Data to Backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Contacts from './components/Contacts';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', 
-      phone: '040-123456', 
-      id: 2 
-    },
-    { name: 'Ada Lovelace', 
-      phone: '39-44-5323523', 
-      id: 3
-    },
-    { 
-      name: 'Dan Abramov', 
-      phone: '12-43-234345', 
-      id: 4 
-    },
-    { name: 'Mary Poppendieck', 
-      phone: '39-23-6423122', 
-      id: 5 
-    }
-  ]);
+
+  const [persons, setPersons] = useState();
 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [newSearch, setNewSearch] = useState("")
+
+  const hook = () => {
+    axios.get('http://localhost:3001/persons').then(response => {
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(hook, [])
 
   const handleSearchChange = (e) => {
     setNewSearch(e.target.value.toLowerCase());
